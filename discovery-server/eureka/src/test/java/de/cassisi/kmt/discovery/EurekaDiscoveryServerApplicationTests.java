1d8bdeeb278c765a0f5aa3364f719c83b790578d
Domenic Cassisi <domenic.cassisi@hs-furtwangen.de>
2021-12-26 11:18:16 +0100

    Bootstrapping config server, discovery server and two trivial services

---
+++ discovery-server/eureka/src/test/java/de/cassisi/kmt/discovery/EurekaDiscoveryServerApplicationTests.java
@@ -0,0 +1,14 @@
+package de.cassisi.kmt.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EurekaDiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
---
+++ microservices/calc-service/src/main/java/de/cassisi/kmt/calculator/CalculatorController.java
@@ -0,0 +1,35 @@
+package de.cassisi.kmt.calculator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CalculatorController {
+
+    private final DiscoveryClient discoveryClient;
+
+    @Value("${myval}")
+    private String myVal;
+
+    public CalculatorController(DiscoveryClient discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(@PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @GetMapping("/")
+    public String getGreeting() {
+        return "Message: " + myVal;
+    }
+
+}
